186bd4d21c420f9c4f4203c1604289bdaf8e4aa3
Prashant P. Khairnar <36958282+prashantkh70@users.noreply.github.com>
2022-07-14 19:03:53 +0530

    Add files via upload
    How to write classes

---
+++ good classes.py
@@ -0,0 +1,16 @@
+
+class Car:
+    #constructor
+    def __init__(self,windows,tyres, engine):
+        self.windows=windows
+        self.tyres=tyres
+        self.engine=engine
+
+    def self_driving(self,engine):
+        print("The car type is {} engine".format(engine))
+
+car1=Car(4,4,"petrol")
+print("The no of typres in object car 1 is {}".format(car1.tyres))
+print("The no of windows in object car 1 is {}".format(car1.windows))
+
+car1.self_driving("electric")
